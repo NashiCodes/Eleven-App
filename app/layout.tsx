6b81d0f6a762f4876b8654e372f5a85d575e9be0
Pereira Dev <nashi.codes@gmail.com>
2025-02-28 00:44:13 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({children,}: Readonly<{ children: React.React
     <head>
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <title>Projeto Estagio App Masters</title>
+        <title>Eleven Apps API Fetcher</title>
     </head>
     <body>
     <ThemeProvider
